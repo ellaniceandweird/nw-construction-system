c3ef4c44ee51d52c7bc49f0067988d32561df4eb
Ella Esquivel <ella@niceandweirdgroup.com>
2026-07-22 03:55:37 +0800

    Remove all stray backup folders

--- src-old-backup-settings/hooks/use-contacts.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useSyncExternalStore } from "react";
-import { subscribeContacts, getContactsSnapshot } from "@/lib/contacts/contact-store";
-import { MOCK_CONTACTS } from "@/lib/data/mock/contacts";
-export function useContacts() {
-  return useSyncExternalStore(subscribeContacts, getContactsSnapshot, () => MOCK_CONTACTS);
-}
