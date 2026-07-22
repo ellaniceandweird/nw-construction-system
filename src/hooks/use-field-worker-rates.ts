23fc98b4f3233b2548b9f8083fc5eb8ff1203ff5
Ella Esquivel <ella@niceandweirdgroup.com>
2026-07-23 03:28:30 +0800

    Add Edit button to Projects list, remove Contract Type/Phase/Priority/Estimated Contract Value from the form

--- src/hooks/use-field-worker-rates.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useSyncExternalStore } from "react";
-import { subscribeFieldWorkerRates, getFieldWorkerRatesSnapshot } from "@/lib/references/field-worker-rate-store";
-import { MOCK_FIELD_WORKER_RATES } from "@/lib/data/mock/field-worker-rates";
-export function useFieldWorkerRates() {
-  return useSyncExternalStore(subscribeFieldWorkerRates, getFieldWorkerRatesSnapshot, () => MOCK_FIELD_WORKER_RATES);
-}
